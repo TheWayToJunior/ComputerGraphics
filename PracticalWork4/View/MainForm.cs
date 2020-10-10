51504c5207922e54609706ecce4970bd2ca03fad
TheWayToJunior <developermicrosoft1932@gmail.com>
2020-10-10 15:42:16 +0300

    Typo fix

--- PracticalWork4/View/MainForm.cs
+++ PracticalWork4/View/MainForm.cs
@@ -9,12 +9,11 @@ namespace PracticalWork4
 {
     public partial class Form1 : Form
     {
+        private int max;
+        private int[] numbers;
+
         private Offset offset;
-
-        int max;
-        int[] numbers;
-
-        Random rnd = new Random((int)DateTime.Now.Ticks);
+        private Random rnd = new Random((int)DateTime.Now.Ticks);
 
         public Form1()
         {
@@ -25,8 +24,8 @@ namespace PracticalWork4
 
             this.Resize += (s, e) => pictureBox1.Refresh();
 
-            numbers = Enumerable.Range(-5, 31).OrderBy(r => rnd.Next()).ToArray();
-            max = numbers.Max();
+            numbers = Enumerable.Range(-12, 31).OrderBy(r => rnd.Next()).ToArray();
+            max = numbers.Max(i => Math.Abs(i));
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -42,13 +41,13 @@ namespace PracticalWork4
             p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
 
             /// TODO: Encapsulate logic in a separate class DrawingGraphics
-            float indentX = (float)(pictureBox1.Width - (24 + 10)) / (numbers.Count() * 10);
+            float indentX = (float)(pictureBox1.Width - (offset.X + 10)) / (numbers.Count() * 10);
             float indentY = (pictureBox1.Height / 2 - 10) / (numbers.Count() * 10f);
 
             for (int i = 1; i < numbers.Count() + 1; i++)
             {
                 float x = i * 10 * indentX;
-                float y = (-numbers[i - 1] * 10) / ((float)max / numbers.Count()) * indentY;
+                float y = -numbers[i - 1] * 10 / ((float)max / numbers.Count()) * indentY;
 
                 listPoints.Add(new PointF(x, y));
 
@@ -63,7 +62,7 @@ namespace PracticalWork4
         {
             var g = e.Graphics;
 
-            var x = new CoordinateAxisX(minValue: 0, maxValue: 31, countPoints: 31);
+            var x = new CoordinateAxisX(minValue: 1, maxValue: 31, countPoints: 31);
             var y = new CoordinateAxisY(minValue: -max, maxValue: max, countPoints: 15);
 
             var cs = new Models.CoordinateSystem(x, y);
